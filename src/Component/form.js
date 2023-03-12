a8585eee9f7108c00f4239aefcbaf44ff1a35e5a
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2023-03-13 01:20:24 +0530

    all validation is done from client side

--- src/Component/form.js
+++ src/Component/form.js
@@ -68,10 +68,79 @@ function Form() {
     const [dob, setDob] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
+    const [age, setAge] = useState('');
+
+    //function to change the name of the user everytime 
+    const handleNameChange = (e) => {
+        console.log("name was changed");
+        console.log(e.target.value);
+        setName(e.target.value)
+    }
+    const handleChangeDob = (e) => {
+        console.log("dob was changed ");
+        console.log(e.target.value);
+        setDob(e.target.value);
+
+        const dobDate = new Date(e.target.value);
+        const ageInMs = Date.now() - dobDate.getTime();
+        const ageDate = new Date(ageInMs);
+        const aage = Math.abs(ageDate.getUTCFullYear() - 1970); // Calculate the age in years
+        setAge(aage);
+        console.log(age);
+
+    }
+
+    const handleChangeEmail = (e) => {
+        console.log(e.target.value)
+        setEmail(e.target.value);
+
+
+    }
+
+    const handleChangePhoneNumber = (e) => {
+        console.log(e.target.value);
+        setPhone(e.target.value);
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+
+        // age validation 
+        if (age < 18) {
+            alert("Sorry, you must be at least 18 years old to proceed");
+            return;
+
+        }
+
+        //mail validation 
+        const re = /\S+@\S+\.\S+/;
+        if (!re.test(email)) {
+            alert('Please enter a valid email address');
+            return;
+        }
+        // phone no validation
+        const phoneRegex = /^\d{10}$/; // regular expression to match 10-digit phone number format
+        if (!phoneRegex.test(phone)) {
+            alert('Phone number must be in 10-digit format');
+            return;
+        }
+
+
+
+
+        console.log("this section is prevented from execution")
+
+
+
+
+
+
+
+
         console.log(name, dob, email, phone);
+
+
     }
 
     return (
@@ -82,7 +151,7 @@ function Form() {
                     type="text"
                     id="name"
                     value={name}
-                    onChange={(e) => setName(e.target.value)}
+                    onChange={handleNameChange}
                     required
                 />
 
@@ -90,18 +159,20 @@ function Form() {
                 <label htmlFor="dob">Date of Birth</label>
                 <input
                     type="date"
+
                     id="dob"
                     value={dob}
-                    onChange={(e) => setDob(e.target.value)}
+                    onChange={handleChangeDob}
                     required
+                    max={new Date().toISOString().split("T")[0]}
                 />
 
                 <label htmlFor="email">Email</label>
                 <input
-                    type="email"
+                    type="text"
                     id="email"
                     value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={handleChangeEmail}
                     required
                 />
 
@@ -110,7 +181,7 @@ function Form() {
                     type="tel"
                     id="phone"
                     value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
+                    onChange={handleChangePhoneNumber}
                     required
                 />
 
